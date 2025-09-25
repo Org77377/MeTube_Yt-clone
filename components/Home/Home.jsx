df6792f9eb20665241cbf4bb3cdcfb5ae65d2e4e
Org77377 <omkarrg111@gmail.com>
2025-09-25 06:01:48 +0530

    video page added

--- components/Home/Home.jsx
+++ components/Home/Home.jsx
@@ -6,7 +6,10 @@ import { useEffect, useState } from "react";
 
 function Home() {
 
+    const[filterd, filterSwitch] = useState(false);
+    const[filset, filSwitch] = useState('');
     const[video, setVideo] = useState([])
+
     useEffect(()=>{
         async function getData(){
             const data = await fetch('http://localhost:3000/', {method: 'GET'});
@@ -16,32 +19,42 @@ function Home() {
         getData();
     },[])
 
+    function setSwitch(data, state){
+        filterSwitch(state);
+        filSwitch(data)
+    }
+
+    const fildata = filterd ? video.videos.filter(data=>data.category.includes(filset)) : video.videos;
+
     return(
+
         <div className="main-container">
+            <div className="filter-btns">
             {video.videos?.map((data)=>
-                <div className="filter-btns">
-                    <button className="filter">{data.category}</button>
-                </div>
-            )}
+                    <button key={data._id} onClick={()=>setSwitch(data.category, !filterd)} className="filter">{data.category}</button>
+                )}
+            </div>
 
             <div className="content-container">
-                {video.videos?.map((data)=>
+                {fildata?.map((data)=>
                 <Link key={data._id} to={`/video/${data._id}`}>
                     <div className="content-box">
+                    <div className="image-wrapper">
                     <img src={data.thumbnailUrl} alt="video-thumbnail" />
+                    </div>
                     <div className="video-info">
                         <div className="channel-avatar">
-                            <img src={data.thumbnailUrl} alt="" />
+                            <img src={video.chInfo?.filter(d=>d.owner.includes(data.uploader)).map(d=>d.channelBanner)} alt="" />
                         </div>
                         <div className="video-details">
                             <span className="vid-title">
                                 {data.title}
                             </span>
                             <span className="channel-name sm-txt">
-                                {data["uploader "]}
+                                {video.uploader?.filter(d=>d._id.includes(data.uploader)).map(d=>d.channel.channelName)}
                             </span>
                             <span className="views-info sm-txt">
-                                {data["views "]} views - {data.uploadDate} 
+                                {data.views} views - {data.uploadDate} 
                             </span>
                         </div>
                     </div>
--- pages/VideoPage.jsx
+++ pages/VideoPage.jsx
@@ -8,21 +8,72 @@ function VideoPage(){
 
     const id = useParams()
     const[video, setVideo] = useState([])
+
         useEffect(()=>{
             async function getData(){
-                const data = await axios.put(`http://localhost:3000/video/like/${id}`);
-                setVideo(data);
+                const data = await fetch(`http://localhost:3000/view/${id.id}`, {method: 'PUT'});
+                const jsonData = await data.json();
+                setVideo(jsonData.data);
             }
             getData();
         },[])
 
-    return(
-        <>
-        <div className="video-container">
-            {console.log(video)}
+    return (
+    <div className="video-page-container">
+      <div className="video-content">
+        {/* Main video section */}
+        <div className="video-player">
+          <div className="video">
+            <iframe
+              src={video.videoUrl} 
+              allow="autoplay;" 
+              allowFullScreen
+              title="Video Player"
+            ></iframe>
+          </div>
+
+          {/* Video description */}
+          <div className="video-description">
+            <h1 className="video-title">{video.title}</h1>
+            <div className="video-meta">
+              <span className="views">{video.views} Views</span> • <span className="publish-date">2 days ago</span>
+            </div>
+            <div className="description-text">
+              <p>
+                {video.description}
+              </p>
+            </div>
+          </div>
+
+          {/* Comments Section */}
+          <div className="video-comments">
+            <h2>Comments</h2>
+            <div className="comment-list">
+              {/* Comments dynamically render here */}
+              <div className="comment">
+                <span className="comment-author">John Doe</span>
+                <p className="comment-text">Great video! Learned a lot from this.</p>
+              </div>
+              {/* More comments */}
+            </div>
+          </div>
         </div>
-        </>
-    )
+
+        {/* Suggested Videos Section */}
+        <div className="suggestions">
+          <h2>Suggested Videos</h2>
+          <div className="suggested-video-thumbnails">
+            {/* You would dynamically load video tiles here */}
+            <div className="video-tile">
+              <img src="thumbnail_url.jpg" alt="video thumbnail" />
+              <p>Suggested Video Title</p>
+            </div>
+            {/* More video tiles */}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default VideoPage;
