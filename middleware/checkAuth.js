8546024575157537822940b111ee68b42d9e4617
Org77377 <omkarrg111@gmail.com>
2025-09-29 21:22:54 +0530

    solve headers issue

--- middleware/checkAuth.js
+++ middleware/checkAuth.js
@@ -2,7 +2,7 @@ import jwt from "jsonwebtoken";
 
 export async function checkAuth(req, res, next){
     try{
-        console.log(req.headers.authorization.split(" ")[1]);
+        // console.log(req.headers.authorization.split(" ")[1]);
         const token = req.headers.authorization.split(" ")[1];
         if(!token){
             return res.status(500).json({msg:"Please login"});
