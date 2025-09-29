6e8b87290bb635c8d4d57aba715667745cdfe9aa
Org77377 <omkarrg111@gmail.com>
2025-09-30 03:48:07 +0530

    context for search video function

--- components/Navbar/SideMenuStatic.jsx
+++ components/Navbar/SideMenuStatic.jsx
@@ -1,15 +1,17 @@
-
 import "../style.css";
+import { Link } from "react-router-dom";
 
 function SideMenuStatic(){
     return(
         <>
             <div className={`side-menu-sm`}>
                 <div className={`side-container-sm`}>
+                        <Link to="/">
                     <div className={`content-sm`}>
                         <i className="bi bi-house-fill"></i>
                         <li>Home</li>
                     </div>
+                        </Link>
                     <div className={`content-sm`}>
                         <i className="bi bi-file-play-fill"></i>
                         <li>Shorts</li>
