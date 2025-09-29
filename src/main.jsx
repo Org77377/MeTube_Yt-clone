291b59d5772f5ba32e30578b12ca19ce558089b1
Org77377 <omkarrg111@gmail.com>
2025-09-29 21:29:26 +0530

    add delete route

--- src/main.jsx
+++ src/main.jsx
@@ -39,7 +39,7 @@ const route = createBrowserRouter([
         element: <Dashboard/>,
       },
       {
-        path: "/editvideo",
+        path: "/dashboard/edit/:id",
         element: <EditVideo/>,
       }
     ]
